05067e226b5470c31d9fde3572bf880c8d5202c4
Labeeb Nassar <132001019@smail.iitpkd.ac.in>
2024-07-10 22:16:41 +0530

    Add files via upload

--- mecanum_bot_description/launch/display.launch.py
+++ mecanum_bot_description/launch/display.launch.py
@@ -11,15 +11,27 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     share_dir = get_package_share_directory('mecanum_bot_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'amr_ust.xacro')
+    xacro_file = os.path.join(share_dir, 'urdf', 'amr_ust.urdf')
     robot_description_config = xacro.process_file(xacro_file)
     robot_urdf = robot_description_config.toxml()
 
-    rviz_config_file = os.path.join(share_dir, 'rviz', 'display.rviz')
+    rviz_config_file = os.path.join(share_dir, 'rviz', 'nice_display.rviz')
 
     gui_arg = DeclareLaunchArgument(
         name='gui',
         default_value='True'
+    )
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='whether to use simulation time or real-sys time'
+    )
+
+    show_display_rviz = DeclareLaunchArgument(
+        'show_display_rviz',
+        default_value='false',
+        description='whether to show robot description rviz view'
     )
 
     show_gui = LaunchConfiguration('gui')
@@ -29,7 +41,10 @@ def generate_launch_description():
         executable='robot_state_publisher',
         name='robot_state_publisher',
         parameters=[
-            {'robot_description': robot_urdf}
+            {
+                'robot_description': robot_urdf,
+                'use_sim_time': LaunchConfiguration('use_sim_time')
+            }
         ]
     )
 
@@ -37,7 +52,10 @@ def generate_launch_description():
         condition=UnlessCondition(show_gui),
         package='joint_state_publisher',
         executable='joint_state_publisher',
-        name='joint_state_publisher'
+        name='joint_state_publisher',
+        parameters=[{
+            'use_sim_time': LaunchConfiguration('use_sim_time')
+        }]
     )
 
     joint_state_publisher_gui_node = Node(
@@ -52,10 +70,16 @@ def generate_launch_description():
         executable='rviz2',
         name='rviz2',
         arguments=['-d', rviz_config_file],
-        output='screen'
+        output='screen',
+        condition=IfCondition(LaunchConfiguration('show_display_rviz')),
+        parameters=[{
+            'use_sim_time': LaunchConfiguration('use_sim_time'),
+        }]
     )
 
     return LaunchDescription([
+        use_sim_time_arg,
+        show_display_rviz,
         gui_arg,
         robot_state_publisher_node,
         joint_state_publisher_node,
