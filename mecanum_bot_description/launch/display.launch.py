31dac7a4fce09978a854bec9e1cbfcefa0c4d844
Labeeb Nassar <132001019@smail.iitpkd.ac.in>
2024-07-18 18:42:35 +0530

    hardware-tested

--- mecanum_bot_description/launch/display.launch.py
+++ mecanum_bot_description/launch/display.launch.py
@@ -11,7 +11,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     share_dir = get_package_share_directory('mecanum_bot_description')
 
-    xacro_file = os.path.join(share_dir, 'urdf', 'amr_ust.urdf')
+    xacro_file = os.path.join(share_dir, 'urdf', 'amr_ust_gen.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     robot_urdf = robot_description_config.toxml()
 
@@ -19,7 +19,7 @@ def generate_launch_description():
 
     gui_arg = DeclareLaunchArgument(
         name='gui',
-        default_value='True'
+        default_value='false'
     )
 
     use_sim_time_arg = DeclareLaunchArgument(
---
+++ mecanum_bot_description/launch/gazebo.launch.py
@@ -0,0 +1,96 @@
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    share_dir = get_package_share_directory('mecanum_bot_description')
+
+    xacro_file = os.path.join(share_dir, 'urdf', 'amr_ust.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_urdf = robot_description_config.toxml()
+    
+    
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='false',
+        description='whether to use simulation time or real-sys time'
+    )
+    
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        parameters=[
+            {'robot_description': robot_urdf, 'use_sim_time': LaunchConfiguration('use_sim_time')}
+        ]
+    )
+
+    # joint_state_publisher_node = Node(
+    #     package='joint_state_publisher',
+    #     executable='joint_state_publisher',
+    #     name='joint_state_publisher'
+    # )
+
+    gazebo_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzserver.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'pause': 'false'
+        }.items()
+    )
+
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzclient.launch.py'
+            ])
+        ])
+    )
+
+    urdf_spawn_node = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', 'amr_ust',
+            '-topic', 'robot_description'
+        ],
+        output='screen'
+    )
+
+    # controller manager nodes
+    # mecanumbot_controller_spawner = Node(
+    #     package='controller_manager',
+    #     executable='spawner',
+    #     arguments=['mecanumbot_controller', '--controller-manager', '/controller_manager']
+    # )
+
+    # joint_broad_spawner = Node(
+    #     package='controller_manager',
+    #     executable='spawner',
+    #     arguments=['joint_broad', '--controller-manager', '/controller_manager']
+    # )
+
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        # joint_state_publisher_node,
+        gazebo_server,
+        gazebo_client,
+        urdf_spawn_node,
+        # joint_broad_spawner,
+        # mecanumbot_controller_spawner
+    ])
