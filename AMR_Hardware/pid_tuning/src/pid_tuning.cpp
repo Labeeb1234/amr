c1bbc1239e578f634ed6f0ce7529c2973a49831d
Labeeb Nassar <labeebranassar@gmail.com>
2025-06-18 13:43:55 +0530

    updated gains

--- AMR_Hardware/pid_tuning/src/pid_tuning.cpp
+++ AMR_Hardware/pid_tuning/src/pid_tuning.cpp
@@ -37,9 +37,12 @@
 #define CONTROL_TIMER 20 //ms
 
 
-float Kp = 2.0;
-float Ki = 0.0;
-float Kd = 0.0;
+// PID tuning params
+static volatile int32_t loop_start = -1; 
+
+float Kp = 5.0;
+float Ki = 0.05;
+float Kd = 2.0;
 
 int total_motors = 4;
 
@@ -73,10 +76,9 @@ Kinematics kinematics(
     WHEELS_DISTANCE_DIFF
 );
 
-
 // a helper function for periodic execution
 #define EXECUTE_EVERY_N_MS(MS, X)  do { \
-  static volatile int16_t init = -1; \
+  static volatile int32_t init = -1; \
   if (init == -1) { init = uxr_millis();} \
   if (uxr_millis() - init > MS) { X; init = uxr_millis();} \
 } while (0)
@@ -85,6 +87,7 @@ Kinematics kinematics(
 float prev_motor_rpm1=0.0, pprev_motor_rpm1=0.0, prev_motor_rpm2=0.0, pprev_motor_rpm2=0.0;
 float prev_motor_rpm3=0.0, pprev_motor_rpm3=0.0, prev_motor_rpm4=0.0, pprev_motor_rpm4=0.0;
 float alpha = 0.2;
+float deadband_threshold = 5.0;
 
 float emea_filter(float& curr_val, float& prev_val){
     curr_val = alpha*curr_val + (1-alpha)*prev_val;
@@ -101,6 +104,14 @@ float running_avg(float& curr_val, float& prev_val, float& prev_prev_val){
 
 // pid runner logic
 void pid_loop(){
+    static unsigned long last_time = 0;
+    unsigned long now = uxr_millis();
+    // Serial.print(">uxr_millis: ");
+    // Serial.print(now);
+    Serial.print(">delta:");
+    Serial.println(now - last_time);
+    last_time = now;
+
     // runing tuning loop at 20ms -- 50Hz same as the main bot loop code for microROS interfacing and data transmission
     // float max_rpm = kinematics.getMaxRPM();
     Kinematics::rpm desired_motor_vel = kinematics.getRPM(0.5, 0.0, 0.0); // + forward motion
@@ -121,38 +132,49 @@ void pid_loop(){
     current_rpm4 = emea_filter(current_rpm4, prev_motor_rpm4);
     // current_rpm4 = running_avg(current_rpm4, prev_motor_rpm4, pprev_motor_rpm4);
 
+    // applying deadband to the motor rpms (for no-load condition it helps (claims to help))
+    // if (abs(desired_motor_vel.motor1 - current_rpm1) < deadband_threshold)
+    //     current_rpm1 = desired_motor_vel.motor1;
+
+    // if (abs(desired_motor_vel.motor2 - current_rpm2) < deadband_threshold)
+    //     current_rpm2 = desired_motor_vel.motor2;
+
+    // if (abs(desired_motor_vel.motor3 - current_rpm3) < deadband_threshold)
+    //     current_rpm3 = desired_motor_vel.motor3;
+
+    // if (abs(desired_motor_vel.motor4 - current_rpm4) < deadband_threshold)
+    //     current_rpm4 = desired_motor_vel.motor4;
+
     // set motor speed based on above bot frame vel
     motor1_controller.spin(motor1_pid.compute(desired_motor_vel.motor1, current_rpm1));
     motor2_controller.spin(motor2_pid.compute(desired_motor_vel.motor2, current_rpm2));
     motor3_controller.spin(motor3_pid.compute(desired_motor_vel.motor3, current_rpm3));
     motor4_controller.spin(motor4_pid.compute(desired_motor_vel.motor4, current_rpm4));
 
-    // for plots
+
+    // // for plots
     Serial.print(">Desired RPM:");
     Serial.println(desired_motor_vel.motor1);
     
-    // Serial.print(">Motor-1 RPM:");
+    // Serial.print(">Filtered Motor-1 RPM:");
     // Serial.println(current_rpm1);
-    // Serial.print(">Motor-2 RPM:");
-    // Serial.println(current_rpm2);
-    // Serial.print(">Motor-3 RPM:");
-    // Serial.println(current_rpm3);
-    // Serial.print(">Motor-4 RPM:");
-    // Serial.println(current_rpm4);
-
-    Serial.print(">Filtered Motor-1 RPM:");
-    Serial.println(current_rpm1);
-    // Serial.print(">Filtered Motor-2 RPM:");
-    // Serial.println(filt_current_rpm2);
+    Serial.print(">Filtered Motor-2 RPM:");
+    Serial.println(current_rpm2);
     // Serial.print(">Filtered Motor-3 RPM:");
-    // Serial.println(filt_current_rpm3);
+    // Serial.println(current_rpm3);
     // Serial.print(">Filtered Motor-4 RPM:");
-    // Serial.println(filt_current_rpm4);
+    // Serial.println(current_rpm4);
+    
 }
 
 void setup(){
     Serial.begin(BAUDRATE);
     initLed();
+
+// #ifdef WDT_TIMEOUT
+//     esp_task_wdt_init(WDT_TIMEOUT, true); //enable panic so ESP32 restarts
+//     esp_task_wdt_add(NULL); //add current thread to WDT watch
+// #endif
 
     motor1_controller.begin();
     motor2_controller.begin();
@@ -164,10 +186,29 @@ void setup(){
     delay(1000);
     setLed(0);
 
+    // loop_start = millis();
 }
 
 void loop(){
+
+    // if(millis()-loop_start > 30000){
+    //     Kp++;
+    //     motor1_pid.updateConstants(Kp, Ki, Kd);
+    //     motor2_pid.updateConstants(Kp, Ki, Kd);
+    //     motor3_pid.updateConstants(Kp, Ki, Kd);
+    //     motor4_pid.updateConstants(Kp, Ki, Kd);
+    //     Serial.print(">Kp:");
+    //     Serial.println(Kp);
+        
+    //     delay(5000);
+    //     esp_restart();
+    // }
+
     EXECUTE_EVERY_N_MS(CONTROL_TIMER, pid_loop());
+
+// #ifdef WDT_TIMEOUT
+//     esp_task_wdt_reset();
+// #endif
 
 }
 
